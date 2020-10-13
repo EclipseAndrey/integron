c6332e0dbbed0eea27ba9cfbdba0cff0d99d49d9
koren <korenkov2002@mail.ru>
2020-10-13 15:22:03 +0300

    Обновление url

--- lib/REST/Autorization/GetCode.dart
+++ lib/REST/Autorization/GetCode.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
+import 'package:omega_qick/REST/Servers.dart';
 
 Future<int> GetCode (String num)async{
-  String urlQuery = "";
+  String urlQuery = serverBD+ "/api.php/checkcode?num=$num";
   print(urlQuery);
   var response;
   try{
