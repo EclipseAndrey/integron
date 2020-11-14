dc30c91642843083ee721d6e44f700130dc006c7
koren <korenkov2002@mail.ru>
2020-11-14 07:26:45 +0300

    Добавлен выход по двойному нажатию

--- lib/Pages/GeneralControllerPages/GeneralControllerPages.dart
+++ lib/Pages/GeneralControllerPages/GeneralControllerPages.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:omega_qick/Contacts/Pages/PageContacts.dart';
 import 'package:omega_qick/EmptyPage.dart';
@@ -28,6 +29,7 @@ class _GeneralControllerPagesState extends State<GeneralControllerPages> {
   PageController controllerPage = PageController(initialPage: 0);
 
 
+  DateTime backbuttonpressTime;
   int _selectedIndex = 0;
 
   @override
@@ -69,82 +71,106 @@ class _GeneralControllerPagesState extends State<GeneralControllerPages> {
 
   double blurSide = 10;
 
+  Future<bool> onWillPop()async{
+    DateTime currentTime = DateTime.now();
+    bool back = backbuttonpressTime == null ||currentTime.difference(backbuttonpressTime)> Duration(seconds: 3);
+
+    if(back){
+      backbuttonpressTime = currentTime;
+      Fluttertoast.showToast(
+          msg: "Нажмите еще раз для выхода",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.SNACKBAR,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.white,
+          textColor: Colors.black,
+          fontSize: 16.0
+      );
+      return false;
+    }
+    return true;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          // IndexedStack(
-          //   index: _selectedIndex,
-          //   children: pageList,
-          // ),
-          PageView(
-            children: pageList,
-            controller: controllerPage,
-            onPageChanged: (index){
-              _onItemTapped(index);
-            },
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Theme(
-              data: Theme.of(context)
-                  .copyWith(canvasColor: cBG.withOpacity(0.7)),
-              child: BottomNavigationBar(
-                elevation: 0,
+      body: WillPopScope(
+        onWillPop: onWillPop,
+        child: Stack(
+          children: [
+            // IndexedStack(
+            //   index: _selectedIndex,
+            //   children: pageList,
+            // ),
+            PageView(
+              children: pageList,
+              controller: controllerPage,
+              onPageChanged: (index){
+                _onItemTapped(index);
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Theme(
+                data: Theme.of(context)
+                    .copyWith(canvasColor: cBG.withOpacity(0.7)),
+                child: BottomNavigationBar(
+                  elevation: 0,
 
-                // backgroundColor: Color.fromRGBO(22, 26, 31, 1),
+                  // backgroundColor: Color.fromRGBO(22, 26, 31, 1),
 
-                items:  <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    activeIcon: getIconForId(id: 52, color: c6287A1),
-                    icon: getIconForId(id: 21, color: c6287A1),
-                    title: Text(
-                      'Главная',
+                  items:  <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      activeIcon: getIconForId(id: 52, color: c6287A1),
+                      icon: getIconForId(id: 21, color: c6287A1),
+                      title: Text(
+                        'Главная',
+                      ),
                     ),
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon: getIconForId(id: 54, color: c6287A1),
-                    icon: getIconForId(id: 49, color: c6287A1),
-                    title: Text(
-                      'Кошельки',
+                    BottomNavigationBarItem(
+                      activeIcon: getIconForId(id: 54, color: c6287A1),
+                      icon: getIconForId(id: 49, color: c6287A1),
+                      title: Text(
+                        'Кошельки',
+                      ),
                     ),
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon: getIconForId(id: 51, color: c6287A1),
-                    icon: getIconForId(id: 55, color: c6287A1),
-                    title: Text(
-                      'Покупки',
+                    BottomNavigationBarItem(
+                      activeIcon: getIconForId(id: 51, color: c6287A1),
+                      icon: getIconForId(id: 55, color: c6287A1),
+                      title: Text(
+                        'Покупки',
+                      ),
                     ),
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon: getIconForId(id: 50, color: c6287A1),
-                    icon: getIconForId(id: 5, color: c6287A1),
-                    title: Text(
-                      'Бизнесы',
+                    BottomNavigationBarItem(
+                      activeIcon: getIconForId(id: 50, color: c6287A1),
+                      icon: getIconForId(id: 5, color: c6287A1),
+                      title: Text(
+                        'Бизнесы',
+                      ),
                     ),
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon: getIconForId(id: 53, color: c6287A1),
-                    icon: getIconForId(id: 48, color: c6287A1),
-                    title: Text(
-                      'Мое',
+                    BottomNavigationBarItem(
+                      activeIcon: getIconForId(id: 53, color: c6287A1),
+                      icon: getIconForId(id: 48, color: c6287A1),
+                      title: Text(
+                        'Мое',
+                      ),
                     ),
-                  ),
-                ],
-                unselectedItemColor: c6287A1,
-                // selectedFontSize: 12,
-                // unselectedFontSize: 12,
-                type: BottomNavigationBarType.fixed,
-                showUnselectedLabels: true,
-                currentIndex: _selectedIndex,
-                selectedItemColor: c6287A1,
-                onTap: bottomTapped,
+                  ],
+                  unselectedItemColor: c6287A1,
+                  // selectedFontSize: 12,
+                  // unselectedFontSize: 12,
+                  type: BottomNavigationBarType.fixed,
+                  showUnselectedLabels: true,
+                  currentIndex: _selectedIndex,
+                  selectedItemColor: c6287A1,
+                  onTap: bottomTapped,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
